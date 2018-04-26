e4ca7310e48118c1491cf6a087314d0f4a85dea0
Neil <neilspurgeon@gmail.com>
2018-04-25 18:02:44 -0700

    init

---
+++ src/components/Header/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import actions from './headerActions.svg';
+import menu from './menuIcon.svg';
+
+
+const Wrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #FFF;
+  width: 100%;
+  height: 65px;
+  border-bottom: 1px #E6E6E6 solid;
+`;
+
+const Title = styled.span`
+  position: absolute;
+  left: 43px;
+  bottom: 14px;
+  line-height: 1;
+  font-size: 1.4rem;
+  letter-spacing: -.015em;
+`
+
+const MenuBttn = styled.span`
+  position: absolute;
+  left: 12px;
+  bottom: 15px;
+  line-height: 0;
+`
+
+const Actions = styled.span`
+  position: absolute;
+  right: 15.5px;
+  bottom: 13.5px;
+  line-height: 0;
+`
+
+const Header = () => {
+  return (
+    <Wrapper>
+      
+      <MenuBttn>
+        <img src={menu} />
+      </MenuBttn>
+      
+      <Title>Getting a Job a...</Title>
+      
+      <Actions>
+        <img src={actions} />
+      </Actions>
+
+    </Wrapper>
+  )
+};
+
+export default Header;
